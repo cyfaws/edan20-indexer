125307128081e2a957af81ec6fe35d4e808d8dc0
Egil <egto6164to-s@student.lu.se>
2019-09-11 19:21:16 +0200

    2

--- index_file.py
+++ index_file.py
@@ -5,25 +5,28 @@ import sys, regex, pickle
 #res = regex.findall(r'\w+', test_string)
 #res = regex.findall(r'\p{Word}+', test_string)
 #print("The list of words is : " + str(res))
+def run(inputfile):
+#inputfile = sys.argv[1]
+#outputfile = sys.argv[2].replace("txt", "idx")
+	outputfile = inputfile.replace(".txt", ".idx")
+	dictionary = {}
+	#file = open(inputfile, "r")
+	file = open(inputfile, "r")
+	text = file.read()
+	#text =  "Geeksforgeeks,    is best @# Computer Science Portal.!!!  is"
 
-inputfile = sys.argv[1]
-outputfile = sys.argv[2].replace("txt", "idx")
+	matches = regex.finditer(r'\p{L}+', text)
+	for match in matches:
+		word = match.group().lower()
+		if dictionary.__contains__(word) :
+		    dictionary[word].append(match.start())
+		else:
+		    dictionary[word] = [match.start()]
 
-dictionary = {}
+	#print(dictionary)
 
-file = open(inputfile,"r")
-text = file.read()
-#text =  "Geeksforgeeks,    is best @# Computer Science Portal.!!!  is"
+	outfile = open(outputfile, 'wb')
+	pickle.dump(dictionary, outfile)
 
-matches = regex.finditer(r'\p{L}+', text)
-for match in matches:
-    word = match.group().lower()
-    if dictionary.__contains__(word) :
-        dictionary[word].append(match.start())
-    else:
-        dictionary[word] = [match.start()]
-
-#print(dictionary)
-
-outfile = open(outputfile, 'wb')
-pickle.dump(dictionary, outfile)
+if __name__ == "__main__":
+   run(sys.argv[1])
--- indexer.py
+++ indexer.py
@@ -1,8 +1,42 @@
-import index_file, folder_reader, os
+import index_file, folder_reader, sys, os, pickle, regex
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+#dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path = sys.argv[1]
+if not dir_path.endswith("/"):
+	dir_path += ("/")
 
-files = folder_reader.get_files(dir_path)
+files = folder_reader.get_files(dir_path, "txt")
 
 for file in files:
-    index_file(file, file)
+    index_file.run(dir_path+file)
+
+indexfiles = folder_reader.get_files(dir_path, "idx")
+master_index = {}
+
+for indexfile in indexfiles:
+	index = pickle.load( open( dir_path+indexfile, "rb" ) )
+
+	entryname = regex.sub(".idx", ".txt", indexfile)
+	for word in index:
+		entry = [{entryname: index[word]}]
+		if master_index.__contains__(word):
+			master_index[word].append(entry)
+		else:
+			master_index[word] = entry
+print(master_index["samlar"])
+
+
+#indices = []
+#for indexfile in indexfiles:
+#	indices.append( pickle.load( open( dir_path+indexfile, "rb" ) ))
+
+#master_index = []
+#for index in indices:
+#	for word in index:
+#		if master_index.__contains__(word) :
+#		    print("")
+#		else:
+#		    master_index[word] = [index.replace(".txt", ""), index[word]]
+#print(master_index)
+
+
