65ed59ebb2ba5e04424d8af3aa67724b4adcf4f0
Egil Tordengren <egiltordengren@gmail.com>
2019-09-12 23:33:54 +0200

    klart

--- index_file.py
+++ index_file.py
@@ -1,32 +1,33 @@
 import sys, regex, pickle
 
-#test_string = "Geeksforgeeks,    is best @# Computer Science Portal.!!!"
-#print("The original string is : " + test_string)
-#res = regex.findall(r'\w+', test_string)
-#res = regex.findall(r'\p{Word}+', test_string)
-#print("The list of words is : " + str(res))
 def run(inputfile):
-#inputfile = sys.argv[1]
-#outputfile = sys.argv[2].replace("txt", "idx")
 	outputfile = inputfile.replace(".txt", ".idx")
 	dictionary = {}
 	#file = open(inputfile, "r")
-	file = open(inputfile, "r")
-	text = file.read()
-	#text =  "Geeksforgeeks,    is best @# Computer Science Portal.!!!  is"
+	file = open(inputfile, "r", encoding='UTF-8')
+	text = file.read().lower()
+
+	wordcount = 0
+	poscount = 0
+
 
 	matches = regex.finditer(r'\p{L}+', text)
 	for match in matches:
-		word = match.group().lower()
-		if dictionary.__contains__(word) :
-		    dictionary[word].append(match.start())
-		else:
-		    dictionary[word] = [match.start()]
 
-	#print(dictionary)
+		word = match.group()#.lower()
+		pos = match.start()
+
+		wordcount += 1
+		poscount += len(word) + 1
+
+		if dictionary.__contains__(word) :
+		    dictionary[word].append(pos)
+		else:
+		    dictionary[word] = [pos]
 
 	outfile = open(outputfile, 'wb')
 	pickle.dump(dictionary, outfile)
+	return wordcount
 
 if __name__ == "__main__":
    run(sys.argv[1])
--- indexer.py
+++ indexer.py
@@ -1,42 +1,86 @@
-import index_file, folder_reader, sys, os, pickle, regex
+import index_file, folder_reader, sys, os, pickle, regex, math
 
-#dir_path = os.path.dirname(os.path.realpath(__file__))
-dir_path = sys.argv[1]
+def calc_tf(word, doc):
+	amount = len(master_index[word][doc])
+	#wordstotal = 0
+	#for wordentry in master_index:
+	#	if master_index[wordentry].__contains__(doc):
+	#		wordstotal += len(master_index[wordentry][doc])
+	#print(wordcounts[doc], wordstotal)
+	return amount / wordcounts[doc]
+
+def calc_idf(word):
+	docs_n = len(files)
+	#docswithword_n = 1
+	docswithword_n = 0
+	if master_index.__contains__(word):
+		docswithword_n += len(master_index[word])
+	return math.log10(docs_n / docswithword_n)
+
+def calc_tfidf(word, doc):
+	return calc_tf(word, doc)*calc_idf(word)
+
+dir_path = "Selma/"
+if len(sys.argv) > 1:
+    dir_path = sys.argv[1]
 if not dir_path.endswith("/"):
 	dir_path += ("/")
 
 files = folder_reader.get_files(dir_path, "txt")
+wordcounts = {}
 
 for file in files:
-    index_file.run(dir_path+file)
+    wordcounts[file] = index_file.run(dir_path+file)
 
 indexfiles = folder_reader.get_files(dir_path, "idx")
 master_index = {}
 
 for indexfile in indexfiles:
 	index = pickle.load( open( dir_path+indexfile, "rb" ) )
-
-	entryname = regex.sub(".idx", ".txt", indexfile)
+	docname = regex.sub(".idx", ".txt", indexfile)
 	for word in index:
-		entry = [{entryname: index[word]}]
+		entry = {docname: index[word]}
 		if master_index.__contains__(word):
-			master_index[word].append(entry)
+			master_index[word][docname] = index[word]
 		else:
 			master_index[word] = entry
-print(master_index["samlar"])
 
+doc_tfidfs = {}
+for master_word in master_index:
+	master_entry = master_index[master_word]
+	for doc in master_entry:
+		if not doc_tfidfs.__contains__(doc):
+			doc_tfidfs[doc] = {}
+		doc_tfidfs[doc][master_word] = calc_tfidf(master_word, doc)
 
-#indices = []
-#for indexfile in indexfiles:
-#	indices.append( pickle.load( open( dir_path+indexfile, "rb" ) ))
+def calc_cos_sim(arr1, arr2):
+	dot = sum(arr1[a]*arr2[b] for a, b in zip(arr1, arr2))
+	#dot = sum([arr1[i] * arr2[i] for i in range(len(arr2))])
+	mag1 = math.sqrt(sum(arr1[a]**2 for a in arr1))
+	mag2 = math.sqrt(sum(arr2[b]**2 for b in arr2))
+	cos = dot / (mag1 * mag2)
+	return cos
 
-#master_index = []
-#for index in indices:
-#	for word in index:
-#		if master_index.__contains__(word) :
-#		    print("")
-#		else:
-#		    master_index[word] = [index.replace(".txt", ""), index[word]]
-#print(master_index)
+cos_sim_matrix_set = {}
+cos_sim_matrix = [[0 for i in range(len(files))] for j in range(len(files))]
 
+i = 0
+for doc1 in files:
+	j = 0
+	for doc2 in files:
+		#cos_sim_matrix[doc1] = {doc2: calc_cos_sim(doc_tfidfs[doc1].values(), doc_tfidfs[doc2].values()) }
+		cos_sim_matrix_set[doc1] = {doc2: calc_cos_sim(doc_tfidfs[doc1], doc_tfidfs[doc2])}
+		print(i,j)
+		cos_sim_matrix[i][j] = cos_sim_matrix_set[doc1][doc2]
+		j += 1
+	i += 1
 
+#	d1	d2	d3	d4
+#d1	x 	12	13	14
+#d2 21	x 	23	24
+#d3	31	32	x 	34
+#d4 41	42	43	x
+print(master_index["nils"]["jerusalem.txt"])
+print(doc_tfidfs["bannlyst.txt"]["et"])
+print(cos_sim_matrix_set)
+print(cos_sim_matrix)
